63e92bbc84327ce9a98318f42acb08c8762b7767
Samuel <samuel@umich.edu>
2019-01-14 13:51:29 -0500

    unbiased estimators and comments

--- tensorflow_snippets/gradient.py
+++ tensorflow_snippets/gradient.py
@@ -1,5 +1,5 @@
 ''' author: samtenka
-    change: 2019-01-12
+    change: 2019-01-13
     create: 2019-01-11
     descrp: Illustrate how to use Tensorflow's automatic differentiation to compute bespoke gradient statistics.
             Given a loss function l(data, weights), where `data` is drawn from a fixed distribution and we imagine
@@ -13,7 +13,7 @@
             loss k times with respect to weights.  The letters are to be read as tensor indices and contracted as usual
             (so, though not used here, a parenthesized expression could actually be a rank-(k minus 2) tensor etc).
             Curly braces indicate an expectation over the data distribution.  As another example, {(aa)} is the trace
-            of the expected hessian.  We call (D) the `ADVENTUROUSNESS` and (E) the `PROGRESSIVENESS`.
+            of the expected hessian.  We call (D) `AUDACITY` and (E) `PASSION`.
 
             For this demonstration, we write data=(noise_a, noise_b) and weights=(weights_a, weights_b), and we set
                 l(data, weights) = (alpha + noise_a) * weights_a + (beta + noise_b) * weights_b**2 
@@ -35,7 +35,7 @@ import numpy as np
 #                            0. HYPERPARAMETERS                               #
 ###############################################################################
 
-BATCH_SIZE = 65536
+BATCH_SIZE = 4096
 ALPHA = 3.0
 BETA  = 2.0
 A = 1.0
@@ -44,7 +44,7 @@ B = 1.0
 
 
 ###############################################################################
-#                            0. READ DATASET                                  #
+#                            1. DEFINE STATISTICS                             #
 ###############################################################################
 
 NoiseA = tf.placeholder(tf.float32, shape=[BATCH_SIZE])
@@ -53,30 +53,47 @@ WeightA = tf.placeholder(tf.float32, shape=[BATCH_SIZE]) # TODO: explain COPYING
 WeightB = tf.placeholder(tf.float32, shape=[BATCH_SIZE])
 Weights = [WeightA, WeightB]
 
-Outputs = (ALPHA + NoiseA) * WeightA + (BETA  + NoiseB) * tf.square(WeightB) + 0.0*tf.reduce_sum(tf.square(Weights)) # TODO: explain Trick
+# use of `Linker` forces tf's gradient computations to return an actual 0 instead of `None` when there is no dependency
+Linker = + 0.0*tf.reduce_sum(tf.square(Weights))
+Outputs = (ALPHA + NoiseA) * WeightA + (BETA  + NoiseB) * tf.square(WeightB) + Linker
+
+BESSEL_FACTOR = BATCH_SIZE / (BATCH_SIZE-1)
+def statistics(x, y): 
+    ''' Return unbiased estimates of
+            PRODUCT_MEAN =  trace({x}{y}),
+            COVARIANCE   =  trace({xy}-{x}{y}), and
+            AVG_PRODUCT  =  trace({xy})
+            PRODUCT_AVG  =  trace({x}{y})
+        for given tensors x and y of shape (nb samples)-by-(dim)
+    '''
+    avg_x = tf.reduce_mean(x, axis=0)
+    avg_y = tf.reduce_mean(y, axis=0) if x is not y else avg_x
+
+    avg_product = tf.reduce_sum(tf.reduce_mean(tf.multiply(x, y), axis=0))
+    product_avg = tf.reduce_sum(tf.multiply(avg_x, avg_y))
+    
+    covariance = BESSEL_FACTOR * (avg_product - product_avg)
+    product_mean = avg_product - covariance
+
+    return product_mean, covariance, avg_product, product_avg
 
 AvgOut = tf.reduce_mean(Outputs)
-Gradients = tf.convert_to_tensor(tf.gradients(Outputs, Weights))
+Gradients = tf.transpose(tf.convert_to_tensor(tf.gradients(Outputs, Weights)))
 AvgGrad = tf.reduce_mean(Gradients, axis=1)
-SqrNorms = tf.reduce_sum(tf.square(Gradients), axis=0)
 
-AvgSqrNorm = tf.reduce_mean(SqrNorms, axis=0)
-SqrNormAvg = tf.reduce_sum(tf.square(AvgGrad), axis=0)
-TraceCovar = (AvgSqrNorm - SqrNormAvg) * BATCH_SIZE / (BATCH_SIZE-1)
-MeanSqrGrad = AvgSqrNorm - TraceCovar
+MeanSqrGrad, TraceCovar, AvgSqrNorm, SqrAvgNorm = statistics(Gradients, Gradients)
 
-GradSqrNormAvg = tf.reduce_sum(tf.convert_to_tensor(tf.gradients(SqrNormAvg, Weights)), axis=1)
-GradAvgSqrNorm = tf.reduce_sum(tf.convert_to_tensor(tf.gradients(AvgSqrNorm, Weights)), axis=1)
-DeltaSqrNormAvg = tf.reduce_sum(tf.math.multiply(AvgGrad, GradSqrNormAvg))
-DeltaAvgSqrNorm = tf.reduce_sum(tf.math.multiply(AvgGrad, GradAvgSqrNorm)) # TODO: oops, this is biased! 
+# below, we multiply by BATCH_SIZE to counter tf.gradients' averaging behavior
+GradMeanSqrGrad = tf.transpose(tf.convert_to_tensor(tf.gradients(MeanSqrGrad, Weights))) * BATCH_SIZE
+GradTraceCovar =  tf.transpose(tf.convert_to_tensor(tf.gradients(TraceCovar, Weights))) * BATCH_SIZE
 
-Adventurousness = (DeltaAvgSqrNorm - DeltaSqrNormAvg) * BATCH_SIZE / (BATCH_SIZE-1)
-Progressiveness = DeltaAvgSqrNorm - Adventurousness
+Passion, _, __, ___ = statistics(Gradients, GradMeanSqrGrad)
+Audacity, _, __, ___ = statistics(Gradients, GradTraceCovar)
 
 
 
 ###############################################################################
-#                            0. READ DATASET                                  #
+#                            2. RUN SESSION                                   #
 ###############################################################################
 
 def get_batch(batch_size=BATCH_SIZE):
@@ -90,10 +107,11 @@ with tf.Session() as session:
     gg  =   session.run(MeanSqrGrad,    feed_dict=batch)
     cc  =   session.run(TraceCovar,     feed_dict=batch)
 
-    prog=   session.run(Progressiveness,feed_dict=batch)
-    advn=   session.run(Adventurousness,feed_dict=batch)
-    print('%.2f --- expected %.2f' % ( out, ALPHA*A + BETA *B**2))
-    print('%.2f --- expected %.2f' % (  gg, ALPHA**2 + 4.0*BETA **2*B**2))
-    print('%.2f --- expected %.2f' % (  cc, 1.0 + 4.0*B**2))
-    print('%.2f --- expected %.2f' % (prog, 16.0*BETA **3*B**2))
-    print('%.2f --- expected %.2f' % (advn, 16.0*BETA *B**2))
+    print('loss         %.2f --- expected %.2f' % ( out, ALPHA*A + BETA *B**2))
+    print('gradient     %.2f --- expected %.2f' % (  gg, ALPHA**2 + 4.0*BETA **2*B**2))
+    print('covariance   %.2f --- expected %.2f' % (  cc, 1.0 + 4.0*B**2))
+
+    pssn=   session.run(Passion,        feed_dict=batch)
+    auda=   session.run(Audacity,       feed_dict=batch)
+    print('passion      %.2f --- expected %.2f' % (pssn, 16.0*BETA **3*B**2))
+    print('adventure    %.2f --- expected %.2f' % (auda, 16.0*BETA *B**2))
