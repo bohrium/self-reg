da53fb73da9c034dacf90af4f73866ba7a0a8485
Samuel <samuel@umich.edu>
2019-08-26 23:07:02 -0400

    3rd order poly prediction

--- experiments/derivator.py
+++ experiments/derivator.py
@@ -1,7 +1,7 @@
 ''' author: samtenka
-    change: 2019-06-16
+    change: 2019-08-26
     create: 2019-06-16
-    descrp: define gradient statistics
+    descrp: define gradient statistics up to 3rd order
 '''
 
 import numpy as np
@@ -190,8 +190,126 @@ def compute_grad_stats(land, N, I=1):
             -     gs.recent('(01-02-13)(03-12)') * (N-1)
         ))
 
-
     return gs
+
+def test_derivator_on_cosh():
+    from cosh_landscapes import Cosh 
+
+    DIM = 256
+    Q = Cosh(dim=DIM)
+    ep = np.exp(1)
+    em = np.exp(-1)
+
+    L = (ep + em)/2
+    G = (ep - em)/2
+    H = (ep + em)/2
+    J = (ep - em)/2
+
+    GG = (ep**2 + em**2)/2
+    HG = (ep**2 - em**2)/2
+    HH = (ep**2 + em**2)/2
+    JG = (ep**2 + em**2)/2
+
+    GGG = (ep**3 - em**3)/2
+    HGG = (ep**3 + em**3)/2
+    HHG = (ep**3 - em**3)/2
+    JGG = (ep**3 - em**3)/2
+
+    HHGG = (ep**4 + em**4)/2
+    JGGG = (ep**4 + em**4)/2
+
+    predictions = {
+        '()(0)': DIM * ( L ),
+        '(01)(0-1)': DIM * ( G**2 ),
+        '(01)(01)':  DIM * ( GG ), 
+        '(01-02)(0-1-2)': DIM * ( H * G**2 ), 
+        '(01-02)(0-12)':  DIM * ( H * GG ),
+        '(01-02)(01-2)':  DIM * ( HG * G  ),
+        '(01-02)(012)':   DIM * ( HGG ),
+        '(01-02-03)(0-1-2-3)': DIM * ( J * G**3 ),
+        '(01-02-03)(0-1-23)':  DIM * ( J * G * GG ),
+        '(01-02-03)(0-123)':   DIM * ( J * GGG ),
+        '(01-02-03)(01-2-3)':  DIM * ( JG * G * G ),
+        '(01-02-03)(01-23)':   DIM * ( JG * GG ),
+        '(01-02-03)(012-3)':   DIM * ( JGG * G ),
+        '(01-02-03)(0123)':    DIM * ( JGGG ),
+        '(01-02-13)(0-1-2-3)': DIM * ( H**2 * G**2 ), 
+        '(01-02-13)(0-1-23)':  DIM * ( H**2 * GG ), 
+        '(01-02-13)(0-12-3)':  DIM * ( H * HG * G ), 
+        '(01-02-13)(0-123)':   DIM * ( H * HGG ), 
+        '(01-02-13)(0-13-2)':  DIM * ( H * HG * G ), 
+        '(01-02-13)(01-2-3)':  DIM * ( HH * G**2 ), 
+        '(01-02-13)(01-23)':   DIM * ( HH * GG ), 
+        '(01-02-13)(012-3)':   DIM * ( HHG * G ), 
+        '(01-02-13)(0123)':    DIM * ( HHGG ), 
+        '(01-02-13)(02-13)':   DIM * ( HG * HG ), 
+        '(01-02-13)(03-12)':   DIM * ( HG * HG ), 
+    }
+
+    grad_stats = str(compute_grad_stats(Q, N=4, I=3000))
+    for name, stats in sorted(eval(grad_stats).items()):
+        mean = stats["mean"]
+        halfrange = 1.96 * stats["stdv"]/stats["nb_samples"]**0.5 
+        print(CC + ' @C \t'.join([
+            'stat @R {:24s}'.format(name),
+            'measured @B {:+8.2f}'.format(mean - halfrange),
+            'to @B {:+8.2f}'.format(mean + halfrange),
+            'expected @Y {:8.2f}'.format(predictions[name]),
+            ' @R violation! ' if halfrange < abs(predictions[name]-mean) else ' @G okay ',
+        '']))
+
+
+def test_derivator_on_quad():
+    from quad_landscapes import Quadratic
+
+    DIM = 256
+    assert DIM%4==0
+    DIM_4 = DIM//4
+
+    A,B,C,D = 0.5, 1.0, 0.1, 0.3
+    hessian    = torch.diag(torch.tensor([A]*DIM_4 + [A]*DIM_4 + [B]*DIM_4 + [B]*DIM_4))
+    covariance = torch.diag(torch.tensor([C]*DIM_4 + [D]*DIM_4 + [D]*DIM_4 + [C]*DIM_4))
+    Q = Quadratic(dim=DIM, hessian=hessian, covariance=covariance)
+
+    predictions = {
+        '()(0)': DIM_4 * (A + B),
+        '(01)(0-1)': DIM_4 * (2*A**2 + 2*B**2),
+        '(01)(01)':  DIM_4 * (2*A**2 + 2*B**2 + 2*C + 2*D),
+        '(01-02)(0-1-2)': DIM_4 * (2*A**3 + 2*B**3),
+        '(01-02)(0-12)':  DIM_4 * (2*A**3 + 2*B**3 + (A+B)*(C+D)),
+        '(01-02)(01-2)':  DIM_4 * (2*A**3 + 2*B**3),
+        '(01-02)(012)':   DIM_4 * (2*A**3 + 2*B**3 + (A+B)*(C+D)),
+        '(01-02-03)(0-1-2-3)': 0.0,
+        '(01-02-03)(0-1-23)':  0.0,
+        '(01-02-03)(0-123)':   0.0,
+        '(01-02-03)(01-2-3)':  0.0,
+        '(01-02-03)(01-23)':   0.0,
+        '(01-02-03)(012-3)':   0.0,
+        '(01-02-03)(0123)':    0.0,
+        '(01-02-13)(0-1-2-3)': DIM_4 * (2*A**4 + 2*B**4),
+        '(01-02-13)(0-1-23)':  DIM_4 * (2*A**4 + 2*B**4 + (A**2+B**2)*(C+D)),
+        '(01-02-13)(0-12-3)':  DIM_4 * (2*A**4 + 2*B**4),
+        '(01-02-13)(0-123)':   DIM_4 * (2*A**4 + 2*B**4 + (A**2+B**2)*(C+D)),
+        '(01-02-13)(0-13-2)':  DIM_4 * (2*A**4 + 2*B**4),
+        '(01-02-13)(01-2-3)':  DIM_4 * (2*A**4 + 2*B**4),
+        '(01-02-13)(01-23)':   DIM_4 * (2*A**4 + 2*B**4 + (A**2+B**2)*(C+D)),
+        '(01-02-13)(012-3)':   DIM_4 * (2*A**4 + 2*B**4),
+        '(01-02-13)(0123)':    DIM_4 * (2*A**4 + 2*B**4 + (A**2+B**2)*(C+D)),
+        '(01-02-13)(02-13)':   DIM_4 * (2*A**4 + 2*B**4),
+        '(01-02-13)(03-12)':   DIM_4 * (2*A**4 + 2*B**4),
+    }
+
+    grad_stats = str(compute_grad_stats(Q, N=4, I=3000))
+    for name, stats in sorted(eval(grad_stats).items()):
+        mean = stats["mean"]
+        halfrange = 1.96 * stats["stdv"]/stats["nb_samples"]**0.5 
+        print(CC + ' @C \t'.join([
+            'stat @R {:24s}'.format(name),
+            'measured @B {:+8.2f}'.format(mean - halfrange),
+            'to @B {:+8.2f}'.format(mean + halfrange),
+            'expected @Y {:8.2f}'.format(predictions[name]),
+            ' @R violation! ' if halfrange < abs(predictions[name]-mean) else ' @G okay ',
+        '']))
 
 if __name__ == '__main__':
 
@@ -204,105 +322,7 @@ if __name__ == '__main__':
     #with open('gs.data', 'w') as f:
     #    f.write(grad_stats)
 
-    from cosh_landscapes import Cosh 
 
-    DIM = 256
-    Q = Cosh(dim=DIM)
-    ep = np.exp(1)
-    em = np.exp(-1)
+    test_derivator_on_cosh()
+    test_derivator_on_quad()
 
-    L = (ep+em)/2
-    G = (ep-em)/2
-    GG = (ep**2 + em**2)/2
-    GGG = (ep**3 - em**3)/2
-    HGG = (ep**3 + em**3)/2
-    HG = (ep**2 - em**2)/2
-    HH = (ep**2 + em**2)/2
-    HHG = (ep**3 - em**3)/2
-    HHGG = (ep**4 + em**4)/2
-    H = (ep+em)/2
-    J = (ep-em)/2
-    JG = (ep**2+em**2)/2
-    JGG = (ep**3-em**3)/2
-    JGGG = (ep**4+em**4)/2
-
-    grad_stats = str(compute_grad_stats(Q, N=4, I=3000))
-    for name, stats in sorted(eval(grad_stats).items()):
-        print(CC + ' @C \t'.join([
-            'stat @R {:24s}'.format(name),
-            'measured @G {:+8.2f}'.format(stats["mean"] - 1.96 * stats["stdv"]/stats["nb_samples"]**0.5),
-            'to @G {:+8.2f}'.format(stats["mean"] + 1.96 * stats["stdv"]/stats["nb_samples"]**0.5),
-            'expected @Y {:8.2f}'.format({
-                '()(0)': DIM * ( L ),
-                '(01)(0-1)': DIM * ( G**2 ),
-                '(01)(01)':  DIM * ( GG ), 
-                '(01-02)(0-1-2)': DIM * ( H * G**2 ), 
-                '(01-02)(0-12)':  DIM * ( H * GG ),
-                '(01-02)(01-2)':  DIM * ( HG * G  ),
-                '(01-02)(012)':   DIM * ( HGG ),
-                '(01-02-03)(0-1-2-3)': DIM * ( J * G**3 ),
-                '(01-02-03)(0-1-23)':  DIM * ( J * G * GG ),
-                '(01-02-03)(0-123)':   DIM * ( J * GGG ),
-                '(01-02-03)(01-2-3)':  DIM * ( JG * G * G ),
-                '(01-02-03)(01-23)':   DIM * ( JG * GG ),
-                '(01-02-03)(012-3)':   DIM * ( JGG * G ),
-                '(01-02-03)(0123)':    DIM * ( JGGG ),
-                '(01-02-13)(0-1-2-3)': DIM * ( H**2 * G**2 ), 
-                '(01-02-13)(0-1-23)':  DIM * ( H**2 * GG ), 
-                '(01-02-13)(0-12-3)':  DIM * ( H * HG * G ), 
-                '(01-02-13)(0-123)':   DIM * ( H * HGG ), 
-                '(01-02-13)(0-13-2)':  DIM * ( H * HG * G ), 
-                '(01-02-13)(01-2-3)':  DIM * ( HH * G**2 ), 
-                '(01-02-13)(01-23)':   DIM * ( HH * GG ), 
-                '(01-02-13)(012-3)':   DIM * ( HHG * G ), 
-                '(01-02-13)(0123)':    DIM * ( HHGG ), 
-                '(01-02-13)(02-13)':   DIM * ( HG * HG ), 
-                '(01-02-13)(03-12)':   DIM * ( HG * HG ), 
-            }[name]),
-        '']))
-
-    #from quad_landscapes import Quadratic
-
-    #DIM = 64
-    #assert DIM%4==0
-    #DIM_4 = DIM//4
-
-    #A,B,C,D = 0.5, 1.0, 0.1, 0.3
-    #hessian    = torch.diag(torch.tensor([A]*DIM_4 + [A]*DIM_4 + [B]*DIM_4 + [B]*DIM_4))
-    #covariance = torch.diag(torch.tensor([C]*DIM_4 + [D]*DIM_4 + [D]*DIM_4 + [C]*DIM_4))
-    #Q = Quadratic(dim=DIM, hessian=hessian, covariance=covariance)
-
-    #grad_stats = str(compute_grad_stats(Q, N=4, I=3000))
-    #for name, stats in sorted(eval(grad_stats).items()):
-    #    print(CC + ' @C \t'.join([
-    #        'stat @R {:24s}'.format(name),
-    #        'measured @G {:+8.2f}'.format(stats["mean"] - 1.96 * stats["stdv"]/stats["nb_samples"]**0.5),
-    #        'to @G {:+8.2f}'.format(stats["mean"] + 1.96 * stats["stdv"]/stats["nb_samples"]**0.5),
-    #        'expected @Y {:8.2f}'.format({
-    #            '()(0)': DIM_4 * (A + B),
-    #            '(01)(0-1)': DIM_4 * (2*A**2 + 2*B**2),
-    #            '(01)(01)':  DIM_4 * (2*A**2 + 2*B**2 + 2*C + 2*D),
-    #            '(01-02)(0-1-2)': DIM_4 * (2*A**3 + 2*B**3),
-    #            '(01-02)(0-12)':  DIM_4 * (2*A**3 + 2*B**3 + (A+B)*(C+D)),
-    #            '(01-02)(01-2)':  DIM_4 * (2*A**3 + 2*B**3),
-    #            '(01-02)(012)':   DIM_4 * (2*A**3 + 2*B**3 + (A+B)*(C+D)),
-    #            '(01-02-03)(0-1-2-3)': 0.0,
-    #            '(01-02-03)(0-1-23)':  0.0,
-    #            '(01-02-03)(0-123)':   0.0,
-    #            '(01-02-03)(01-2-3)':  0.0,
-    #            '(01-02-03)(01-23)':   0.0,
-    #            '(01-02-03)(012-3)':   0.0,
-    #            '(01-02-03)(0123)':    0.0,
-    #            '(01-02-13)(0-1-2-3)': DIM_4 * (2*A**4 + 2*B**4),
-    #            '(01-02-13)(0-1-23)':  DIM_4 * (2*A**4 + 2*B**4 + (A**2+B**2)*(C+D)),
-    #            '(01-02-13)(0-12-3)':  DIM_4 * (2*A**4 + 2*B**4),
-    #            '(01-02-13)(0-123)':   DIM_4 * (2*A**4 + 2*B**4 + (A**2+B**2)*(C+D)),
-    #            '(01-02-13)(0-13-2)':  DIM_4 * (2*A**4 + 2*B**4),
-    #            '(01-02-13)(01-2-3)':  DIM_4 * (2*A**4 + 2*B**4),
-    #            '(01-02-13)(01-23)':   DIM_4 * (2*A**4 + 2*B**4 + (A**2+B**2)*(C+D)),
-    #            '(01-02-13)(012-3)':   DIM_4 * (2*A**4 + 2*B**4),
-    #            '(01-02-13)(0123)':    DIM_4 * (2*A**4 + 2*B**4 + (A**2+B**2)*(C+D)),
-    #            '(01-02-13)(02-13)':   DIM_4 * (2*A**4 + 2*B**4),
-    #            '(01-02-13)(03-12)':   DIM_4 * (2*A**4 + 2*B**4),
-    #        }[name]),
-    #    '']))
--- experiments/predictor.py
+++ experiments/predictor.py
@@ -8,6 +8,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 from optimlogs import OptimKey
 import sys 
+from utils import prod
 
 red  ='#cc4444'
 green='#44cc44'
@@ -21,11 +22,16 @@ def from_string(gradstats, formula, eta, T):
         S = S.replace(k, 'gradstats["{}"]["stdv"]/gradstats["{}"]["nb_samples"]**0.5'.format(k, k))
     return (eval(form, {'eta':eta, 'T':T, 'gradstats':gradstats, 'np':np}) for form in (Y, S))
 
+def choose(T, t):
+    return prod(range(T-t+1, T+1)) / float(prod(range(1, t+1)))
+
 sgd_test_coeffs = (
-    '(+ (()(0)))',
-    '(- (T*(01)(0-1)))',
-    '(+ ((T*(T-1)/2.0)*2*(01-02)(0-1-2) + T*0.5*(01-02)(0-12)))',
+    '(+ ( ()(0) ) )',
+    '(- (choose(T, 1) * (01)(0-1)) )',
+    '(+ (choose(T, 2) * 2*(01-02)(0-1-2) + choose(T, 1) * (01-02)(0-12)) )',
+    '(- (choose(T, 3) * (4*(01-02-13)(0-1-2-3) + 2*(01-02-03)(0-1-2-3)) + choose(T, 2) * (1.5 * (01-02-03)(01-23) + (01-02-13)(0-12-3)) + choose(T, 1) * ((1.0/6) * (01-02-03)(0-123))) )',
 )
+
 def sgd_test_taylor(gradstats, eta, T, degree):
     assert 1 <= degree, 'need strictly positive degree of approximation!'
     formula = ' + '.join('eta*'*d + sgd_test_coeffs[d] for d in range(degree+1))
